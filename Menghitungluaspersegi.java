e24c8c5bc13cc0d8bf7526dd5bbb26d877dbdfff
hendisaogo09 <162803597+hendisaogo09@users.noreply.github.com>
2024-03-09 19:01:38 +0700

    Add files via upload

---
+++ Menghitungluaspersegi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menghitungluaspersegi;
+
+/**
+ *
+ * @author ELVINDO WIBOWO
+ */
+public class Menghitungluaspersegi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("masukkan alas segita:");
+        
+        
+        System.out.print("masukkan tinggi segitiga:'");
+        String luas = null;
+        
+        
+        System.out.println("Luas segitiga adalah:" +luas);
+        
+        
+        
+    }
+    
+}
